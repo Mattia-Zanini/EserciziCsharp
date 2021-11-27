227b23d2557f655355de504e7114acc7c81a3827
Mattia-Zanini <mattzanini2@gmail.com>
2021-11-27 18:27:12 +0100

    aggiunti alcuni es

---
+++ Csharp_Call_Python/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esecuzione del programma python");
+            run_cmd();
+        }
+        private static void run_cmd()
+        {
+
+            string fileName = @"C:\Users\Mattia\Desktop\Csharp_Call_Python\prova.py";
+
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"C:\Python310\python.exe", fileName)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine(output);
+        }
+    }
+}
