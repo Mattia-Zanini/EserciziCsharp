3814216496d9ad9621cb78bfd8b2d889fb21d4f2
Mattia-Zanini <mattzanini2@gmail.com>
2021-12-17 09:26:29 +0100

    Funzione generica

--- Poliformismo/Program.cs
+++ Poliformismo/Program.cs
@@ -6,47 +6,13 @@ namespace Poliformismo
     {
         static void Main(string[] args)
         {
-            string nome, cognome, matricola;
             int ore1 = 0, ore2 = 0, ore3 = 0;
-            Console.WriteLine($"Dati dell'operaio");
-            Console.WriteLine("Nome:");
-            nome = Console.ReadLine();
-            Console.WriteLine("Cognome");
-            cognome = Console.ReadLine();
-            Console.WriteLine("Matricola");
-            matricola = Console.ReadLine();
-            Operaio o = new Operaio()
-            {
-                nome = nome,
-                cognome = cognome,
-                matricola = matricola
-            };
-            Console.WriteLine($"Dati dell'impiegato");
-            Console.WriteLine("Nome:");
-            nome = Console.ReadLine();
-            Console.WriteLine("Cognome");
-            cognome = Console.ReadLine();
-            Console.WriteLine("Matricola");
-            matricola = Console.ReadLine();
-            Impiegato i = new Impiegato()
-            {
-                nome = nome,
-                cognome = cognome,
-                matricola = matricola
-            };
-            Console.WriteLine($"Dati del dirigente");
-            Console.WriteLine("Nome:");
-            nome = Console.ReadLine();
-            Console.WriteLine("Cognome");
-            cognome = Console.ReadLine();
-            Console.WriteLine("Matricola");
-            matricola = Console.ReadLine();
-            Dirigente d = new Dirigente()
-            {
-                nome = nome,
-                cognome = cognome,
-                matricola = matricola
-            };
+            Operaio o = new Operaio();
+            Impiegato i = new Impiegato();
+            Dirigente d = new Dirigente();
+            GetDati<Operaio>(ref o, "operaio");
+            GetDati<Impiegato>(ref i, "impiegato");
+            GetDati<Dirigente>(ref d, "dirigente");
             Console.WriteLine("Orario dell'operaio:");
             ore1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Orario dell'impiegato:");
@@ -56,6 +22,20 @@ namespace Poliformismo
             Console.WriteLine($"OPERAIO\nNome: {o.nome}\nCognome: {o.cognome}\nMatricola: {o.matricola}\nRetribuzione oraria: {o.RetribuzioneOraria(ore1)}");
             Console.WriteLine($"IMPIEGATO\nNome: {i.nome}\nCognome: {i.cognome}\nMatricola: {i.matricola}\nRetribuzione oraria: {i.RetribuzioneOraria(ore2)}");
             Console.WriteLine($"DIRIGENTE\nNome: {d.nome}\nCognome: {d.cognome}\nMatricola: {d.matricola}\nRetribuzione oraria: {d.RetribuzioneOraria(ore3)}");
+        }
+        private static void GetDati<T>(ref T dipendete, string nomeDip) where T : Dipendente
+        {
+            string nome, cognome, matricola;
+            Console.WriteLine($"Dati {nomeDip}");
+            Console.WriteLine("Nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Cognome");
+            cognome = Console.ReadLine();
+            Console.WriteLine("Matricola");
+            matricola = Console.ReadLine();
+            dipendete.nome = nome;
+            dipendete.cognome = cognome;
+            dipendete.matricola = matricola;
         }
     }
 }
