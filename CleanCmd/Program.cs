6f54de01689f36c451fd1f842790b1ef3307eb4d
Mattia-Zanini <mattzanini2@gmail.com>
2022-05-29 14:18:37 +0200

    .

---
+++ CleanCmd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CleanCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            List<string> output = new List<string>();
+            lines = File.ReadAllLines("comandi.txt").ToList();
+            foreach (string line in lines)
+            {
+                string[] item = line.Split(' ');
+                string row = "";
+                for(int i = 5; i < item.Length; i++)
+                {
+                    if (i != item.Length - 1)
+                    {
+                        row += item[i] + " ";
+                    }
+                    else
+                    {
+                        row += item[i];
+                    }
+                }
+                output.Add(row);
+            }
+            File.WriteAllLines("output.txt", output);
+        }
+    }
+}
